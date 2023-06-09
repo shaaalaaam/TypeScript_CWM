618d7cd4a0b7fdb69659aef1628b81f901d246a0
shaaalaaam <shaaalaaam@gmail.com>
2023-06-09 10:00:03 +0530

    Update

--- dist/index.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-let age = 20;
-console.log("Hello World ,My age is ");
-//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 let age: number =20;
+if(age>10)
+age+=10;
 console.log("Hello World ,My age is ");
