6a748b8958653b8d4e639e8d43664d3f12b88cc8
shalam <73836150+shaaalaaam@users.noreply.github.com>
2023-06-11 10:58:03 +0530

    update
    upda

---
+++ dist/index.js
@@ -0,0 +1,19 @@
+
+
+
+// "use strict";
+// function CalWeight(weight) {
+
+// }    if (typeof weight === 'number') {
+//         return weight * 2.2;
+//     }
+//     else {
+//         return parseInt(weight) * 2.2;
+//     }
+// console.log(CalWeight(10));
+// console.log(CalWeight('10'));
+// let age: number =20;
+// if(age>10)
+// age+=10;
+// console.log("Hello World ,My age is ");
+//# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,47 @@
-let age: number =20;
-if(age>10)
-age+=10;
-console.log("Hello World ,My age is ");
+
+
+
+ //literal
+//  let num :50 | 100=; 
+
+
+
+
+
+
+
+
+
+//intersection
+
+// type resizable={
+//     resize: ()=> void;
+// }
+// type draggable={
+//     drag: ()=>void;
+// }
+// type UIwidget= resizable & draggable;
+
+// let textbox : UIwidget={
+//     resize: ()=> {},
+//     drag: ()=> {}
+// }
+
+
+// function CalWeight(weight: number | string): number{
+//     if( typeof weight === 'number'){
+//         return weight*2.2;
+//     }else{
+//         return parseInt(weight)*2.2;
+//     }
+// }
+
+// console.log(CalWeight(10));
+// console.log(CalWeight('10'));
+
+
+
+// let age: number =20;
+// if(age>10)
+// age+=10;
+// console.log("Hello World ,My age is ");
